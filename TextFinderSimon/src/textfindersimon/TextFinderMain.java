d933882b91d57bd4360b1e75fb5e53e7955e2fc4
sfv02 <sfv02@DESKTOP-PNR47NH>
2019-10-03 11:23:48 -0600

    Commit inicial.

---
+++ TextFinderSimon/src/textfindersimon/TextFinderMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textfindersimon;
+
+/**
+ *
+ * @author sfv02
+ */
+public class TextFinderMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
